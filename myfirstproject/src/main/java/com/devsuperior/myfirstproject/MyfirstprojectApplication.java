94666c03db19649004ffd16cf1d8e62858e28372
CIANDT\hamaraisa <hamaraisa@ciandt.com>
2022-07-04 09:04:16 -0300

    Aula do professor Nélio Alves - JPA / Springboot

---
+++ myfirstproject/src/main/java/com/devsuperior/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,30 @@
+package com.devsuperior.myfirstproject;
+
+import com.devsuperior.myfirstproject.entities.Category;
+import com.devsuperior.myfirstproject.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+			Category cat1 = new Category(1L, "Eletronics");
+			Category cat2 = new Category(2L, "Books");
+			Category cat3 = new Category(3L, "Automotive");
+
+			categoryRepository.save(cat1);
+			categoryRepository.save(cat2);
+			categoryRepository.save(cat3);
+
+	}
+}
